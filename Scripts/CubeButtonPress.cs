1750995e14c0890fd94bffbb970f69b1985632bb
Matthew Donald Stevenson <mattdst@hotmail.com>
2018-03-02 15:20:59 -0700

    Upload

---
+++ Scripts/CubeButtonPress.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeButtonPress : MonoBehaviour
+{
+
+    private Renderer rend;
+    private Color currentColor;
+    private void OnCollisionEnter(Collision collision)
+    {
+        rend = transform.parent.Find("Light").GetComponent<Renderer>();
+        currentColor = rend.material.GetColor("_Color");
+        if (currentColor == Color.red)
+        {
+            rend.material.SetColor("_Color", Color.green);
+        }
+        else if(currentColor == Color.green)
+        {
+            rend.material.SetColor("_Color", Color.red);
+        }
+    }
+}
